415f31fbd2bac0db215134c3d71defb8d84b6276
RPsaila <RPsaila@users.noreply.github.com>
2019-03-09 03:01:58 -0500

    Initial Commit
    Basic VR scene with Script for Enemy Spawning and Movement.
    To test the scene & scripts you must touch the cube placed in the corridor with the camera while moving it with your keyboard.
    
    NEXT IMPLEMENTATIONS
    
    MAJOR NEEDS
    - Stats system for transcript results
    - Menu & Setup Systems
    - More interactions between Player and GameObjects
    
    SMALL CHANGES
    - Multiple Spawn Points
    - Activate Enemy depending on distance to play as well as delimit score system for how well he performed
    - Increment Enemy model and animations

---
+++ Multi-Tasker Master/Assets/Our Assets/Scripts/Tool1.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tool1 : MonoBehaviour
+{
+    EnemyMove myEnemyMoveScript;
+    public GameObject Spawn;
+
+    public List<GameObject> enemies;
+
+    // Start is called before the first frame update
+
+    IEnumerator Start()
+    {
+        myEnemyMoveScript = Spawn.GetComponent<EnemyMove>();
+        enemies = myEnemyMoveScript.enemies;
+        yield return new WaitForEndOfFrame();
+    }
+
+    // Update is called once per frame
+    public void FixedUpdate()
+    {
+      
+    }
+
+    private void OnCollisionEnter(Collision enemy)
+    {
+        //Debug.Log("works");
+        if (enemy.gameObject.tag == "MovingEnemy")
+        {
+            //Debug.Log("enemycollision");
+            for (int i = enemies.Count - 1; i >= 0; i--)
+            {
+                //Debug.Log(i);
+                if (enemies[i].gameObject.Equals(enemy.gameObject))
+                {
+                    //Debug.Log("enemycollision detected");
+                    enemies.Remove(enemies[i].gameObject);
+                    Destroy(enemy.gameObject);
+                    // You killed my father, prepare to die!
+                }
+            }
+        }
+    }
+}
